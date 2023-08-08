4ba339361221204fb69d949ed2daf60ec40c7018
Matthew Smilansky <msmilansky@gmail.com>
2023-08-07 21:16:43 -0700

    chore: replace square sprite with character sprite

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
 import logoImg from './assets/logo.png';
 import backgroundImg from './assets/background.png';
-import squareImg from './assets/square.png';
+import characterImg from './assets/character.png';
 
 class MyGame extends Phaser.Scene {
 	constructor() {
@@ -10,35 +10,39 @@ class MyGame extends Phaser.Scene {
 
 	preload() {
 		this.load.image('background', backgroundImg);
-		this.load.image('square', squareImg);
+		this.load.image('character', characterImg);
 	}
 
 	create() {
 		this.createBg();
-		this.createSquare();
+		this.createCharacter();
 	}
 	createBg() {
 		this.add.image(0, 0, 'background').setOrigin(0, 0);
 	}
 
-	createSquare() {
-		this.physics.add.sprite(config.squareStartPosition.x, config.squareStartPosition.y, 'square').setOrigin(0.5, 0.5);
+	createCharacter() {
+		this.physics.add
+			.sprite(config.characterStartPosition.x, config.characterStartPosition.y, 'character')
+			.setOrigin(0.5, 0.5);
 	}
 }
 
-const WIDTH = 320;
-const HEIGHT = 240;
-const SQUARE_POSITION = { x: WIDTH * 0.5, y: HEIGHT / 2 };
+const WIDTH = 800;
+const HEIGHT = 600;
+const CHARACTER_POSITION = { x: WIDTH * 0.5, y: HEIGHT / 2 };
 
 const sharedConfig = {
 	width: WIDTH,
 	height: HEIGHT,
-	squareStartPosition: SQUARE_POSITION,
+	characterStartPosition: CHARACTER_POSITION,
 };
 const config = {
+	/*
 	scale: {
 		mode: Phaser.Scale.FIT,
 	},
+	*/
 	type: Phaser.AUTO,
 	...sharedConfig,
 	physics: {
